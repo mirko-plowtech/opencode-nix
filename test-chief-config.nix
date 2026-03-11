cdcc7af2f4db1116d74104169fcbf3e49286116f
mk <mirko.alic@plowtech.net>
2026-03-11 17:15:25 -0300

    Add proper checks

--- test-chief-config.nix
+++
@@ -1,9 +0,0 @@
-let
-  flake = builtins.getFlake (toString ./.);
-  pkgs = import <nixpkgs> {
-    overlays = [ flake.overlays.default ];
-  };
-in
-  pkgs.lib.opencode.mkOpenCodeConfig [
-    flake.nixosModules.example-chief-coding-assistant
-  ]
